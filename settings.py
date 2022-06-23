54d9b977f94583ecd8a702b3e9cd6c0bec642966
mamolina01 <matiasnmolina1@gmail.com>
2022-06-22 21:06:25 -0300

    todo funcionando

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@
 import dj_database_url  
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
